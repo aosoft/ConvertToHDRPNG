7d0db1897948f90b3d4ba8188703fa937deabf37
Yasuhiro Taniuchi <tan-y@aosoft.jp>
2021-03-07 14:59:39 +0900

    Implement Gradation function

---
+++ ConvertToHDRPNG/Color.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertToHDRPNG
+{
+    readonly struct Color
+    {
+        public float R { get; }
+        public float G { get; }
+        public float B { get; }
+
+        public Color(float r, float g, float b)
+        {
+            R = r;
+            G = g;
+            B = b;
+        }
+
+        public static Color operator +(in Color a, in Color b) => new Color(a.R + b.R, a.G + b.G, a.B + b.B);
+
+        public Color Rec709ToRec2020() => new Color(
+            0.627402f * R + 0.329292f * G + 0.043306f * B,
+            0.069095f * R + 0.919544f * G + 0.011360f * B,
+            0.016394f * R + 0.088028f * G + 0.895578f * B);
+
+        private const double MagickScale = 65535.0;
+        private const double HdrScale = 80.0 / 10000.0;
+
+        public Color LinearToST2084()
+        {
+            static double fn(double color)
+            {
+                color /= MagickScale;
+                var m1 = 2610.0 / 4096.0 / 4;
+                var m2 = 2523.0 / 4096.0 * 128;
+                var c1 = 3424.0 / 4096.0;
+                var c2 = 2413.0 / 4096.0 * 32;
+                var c3 = 2392.0 / 4096.0 * 32;
+                var cp = Math.Pow(Math.Abs(color * HdrScale), m1);
+                return Math.Pow((c1 + c2 * cp) / (1 + c3 * cp), m2) * MagickScale;
+            }
+
+            return new Color((float)fn(R), (float)fn(G), (float)fn(B));
+        }
+    }
+}
--- ConvertToHDRPNG/Program.cs
+++ ConvertToHDRPNG/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Cocona;
+using ImageMagick;
+
 
 namespace ConvertToHDRPNG
 {
@@ -6,7 +10,62 @@ namespace ConvertToHDRPNG
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CoconaLiteApp.Run<Program>(args);
         }
+
+        public void Convert(string input, string output)
+        {
+        }
+
+        public void Gradation(string output, int width = 1024, int height = 1024, float start = 0.0f, float end = 1.0f) =>
+            Process(output, width, height, (dstPixels, bt2100pq) =>
+            {
+                Parallel.For(0, height, y =>
+                {
+                    var yy = (float)y / height;
+                    Func<float, Color> fn;
+                    Func<float, Color> fn2;
+                    if (yy < 0.25)
+                    {
+                        fn2 = f => new Color(f, 0, 0);
+                    }
+                    else if (yy < 0.5)
+                    {
+                        fn2 = f => new Color(0, f, 0);
+                    }
+                    else if (yy < 0.75)
+                    {
+                        fn2 = f => new Color(0, 0, f);
+                    }
+                    else
+                    {
+                        fn2 = f => new Color(f, f, f);
+                    }
+                    if (bt2100pq)
+                    {
+                        fn = f => fn2(f).Rec709ToRec2020().LinearToST2084();
+                    }
+                    else
+                    {
+                        fn = fn2;
+                    }
+                    for (int x = 0; x < width; x++)
+                    {
+                        var dstColor = fn((start + x * (end - start) / width) * 65535.0f);
+                        dstPixels.SetPixel(new Pixel(x, y, new float[] { dstColor.R, dstColor.G, dstColor.B }));
+                    }
+                });
+            });
+
+        private void Process(string output, int width, int height, Action<IPixelCollection<float>, bool> fn)
+        {
+            using var dst = new MagickImage(MagickColor.FromRgb(0, 0, 0), width, height);
+            bool exr = string.Equals(".exr", System.IO.Path.GetExtension(output), StringComparison.OrdinalIgnoreCase);
+
+            dst.ColorSpace = exr ? ColorSpace.RGB : ColorSpace.sRGB;
+            fn(dst.GetPixels(), !exr);
+            dst.Write(output, exr ? MagickFormat.Exr : MagickFormat.Png48);
+        }
+
     }
 }
