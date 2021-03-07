ac7777659dbdd7d9b10933a9447f5232a7833ee6
Yasuhiro Taniuchi <tan-y@aosoft.jp>
2021-03-07 11:30:08 +0900

    プロジェクト ファイルを追加します。

---
+++ ConvertToHDRPNG/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConvertToHDRPNG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
